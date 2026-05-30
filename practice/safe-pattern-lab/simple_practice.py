d0b91e5b043147ed879a4630ff8b5e0040c98757
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-05-29 21:15:02 -0700

    .

---
+++ practice/safe-pattern-lab/simple_practice.py
@@ -0,0 +1,42 @@
+
+
+fruits = ["apple", "banana", "cherry"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+# for fruit in fruits:
+#     if fruit.startswith("b"):
+#         print(fruit)
+
+
+
+
+# nums = [3, 5, 2]
+# total = 0
+
+# for n in nums:
+#     total = total + n
+
+# print(total)
+
+# nums = [4, 12, 7, 19, 3, 15]
+
+# for n in nums:
+#     if n > 10:
+#         print(n)
+
+# words = ["hi", "there", "friend"]
+# for w in words:
+#     print(w.upper())
+
+nums = [2, 5, 8, 11, 14, 17]
+# for n in nums:
+#     if n % 2 ==1:
+#         print(n)
+
+for n in nums:
+    if n % 2 == 1:
+        if n > 5:
+            print(n)
