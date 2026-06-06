60f4043372a260d21f61053ecd8b8a8fe9f404e8
Junkfoodbrain <lsorensen1@byupathway.edu>
2026-06-05 23:29:16 -0700

    final push for siteplan.

---
+++ finalproject/scripts/siteplan.js
@@ -0,0 +1,4 @@
+const today = new Date();
+today.getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById("currentyear").textContent = today.getFullYear();
